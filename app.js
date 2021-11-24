b60ca31edd99b2b3089039240a4116d5b3b191ba
harmeetsingh029 <singh.h1995@gmail.com>
2021-11-24 09:31:06 -0600

    intiial board setup and intializer function

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello')
+let gameState = {
+    players: ['x', 'o'],
+    board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ]
+}
+
+function buildInitialState(){
+    for(let i = 0; i < 3; i++){
+        for(let k = 0; k < 3; k++){
+            gameState.board[i][k] = null
+        }
+    }
+}
+
+let currentPlayer = gameState.players[0]
