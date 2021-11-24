f7433a95382346842fddc4877466317eb883fa28
harmeetsingh029 <singh.h1995@gmail.com>
2021-11-24 11:16:58 -0600

    added td ids and getnames function, updating renderstate

--- app.js
+++ app.js
@@ -7,12 +7,51 @@ let gameState = {
     ]
 }
 
+let currentPlayer = gameState.players[0]
+
 function buildInitialState(){
     for(let i = 0; i < 3; i++){
         for(let k = 0; k < 3; k++){
             gameState.board[i][k] = null
         }
+    }  
+}
+
+function swapPlayer(){
+    if(currentPlayer === 'x'){
+        currentPlayer = gameState.players[1]
+    }
+    else{
+        currentPlayer = gameState.players[0]
     }
 }
 
-let currentPlayer = gameState.players[0]
+
+
+
+function getNames(){
+    let playerNameOne = document.getElementById("playerOne")
+    let playerNameTWo = document.getElementById("playerTwo")
+    //alert(playerNameOne.value + playerNameTWo.value)
+}
+
+function renderState(){
+    if(gameState.board)
+    gameState.board[0][0] = currentPlayer.toString()
+    swapPlayer()
+}
+
+function onBoardClick(event){
+    let tdCell = event.target
+    //alert(event.target.className)
+    cell = event.target.className
+
+    if(!(tdCell.className.includes("taken"))){
+        tdCell.classList.add("taken")
+        tdCell.innerText = currentPlayer.toString()
+        renderState()
+    }
+}
+
+let board = document.getElementById('board')
+board.addEventListener('click', onBoardClick)
